1ff9f2cef04553f230ce09ffd3c2bda980ed57de
Saran K <hiclashersaran@gmail.com>
2026-08-06 12:31:17 +0530

    feat: Refactor application structure with routing and new components

--- vector/src/App.jsx
+++ vector/src/App.jsx
@@ -1,44 +1,15 @@
-import './App.css'
-import AnimatedSection from './components/AnimatedSection'
-import CompetitionSection from './components/CompetitionSection'
-import FaqSection from './components/FaqSection'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import { useLocation } from 'react-router-dom'
-import IntroSection from './components/IntroSection'
-import MissionSection from './components/MissionSection'
-import ScheduleSection from './components/ScheduleSection'
-import SponsorsSection from './components/SponsorsSection'
-import TracksSection from './components/TracksSection'
-import VenueSection from './components/VenueSection'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import HomePage from './components/HomePage'
 import VolunteersPage from './components/VolunteersPage'
 
-function App() {
-  const location = useLocation()
-  const isHome = location.pathname === '/'
-
+export default function App() {
   return (
-    <main>
-      <Header />
-      {isHome ? (
-        <>
-          <Hero />
-          <AnimatedSection><IntroSection /></AnimatedSection>
-          <MissionSection />
-          <AnimatedSection><CompetitionSection /></AnimatedSection>
-          <TracksSection />
-          <AnimatedSection><ScheduleSection /></AnimatedSection>
-          <AnimatedSection><VenueSection /></AnimatedSection>
-          <AnimatedSection><FaqSection /></AnimatedSection>
-          <AnimatedSection><SponsorsSection /></AnimatedSection>
-        </>
-      ) : (
-        <VolunteersPage />
-      )}
-      <Footer />
-    </main>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/volunteers" element={<VolunteersPage />} />
+      </Route>
+    </Routes>
   )
 }
-
-export default App
